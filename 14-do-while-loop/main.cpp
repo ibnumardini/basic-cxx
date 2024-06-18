9fccab1d50638f8bb29da7da258d5d69deea4a47
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-06-18 15:05:48 +0700

    feat: do while loop

---
+++ 14-do-while-loop/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+
+    do
+    {
+        cout << "hore " << a << endl;
+        a++;
+    } while (a <= 5);
+
+    cout << "selesai" << endl;
+
+    return 0;
+}
