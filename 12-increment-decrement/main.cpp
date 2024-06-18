a720404a58adc366453fb88ab3df277b27683544
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-06-18 14:52:04 +0700

    feat: increment and decrement

---
+++ 12-increment-decrement/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+
+    // ada 2 tipe, pre & post, pre itu sebelum dan post itu sesudah
+
+    cout << a << endl;
+    cout << ++a << endl;
+    cout << a++ << endl;
+    cout << a << endl;
+
+    cout << a << endl;
+    cout << --a << endl;
+    cout << a-- << endl;
+    cout << a << endl;
+
+    return 0;
+}
