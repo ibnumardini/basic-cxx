e28426584974a46dcb644e1cfd270c886143e869
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-06-18 14:48:13 +0700

    feat: input data from console

---
+++ 4-deklarasi/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    a = 10;
+    cout << a << endl;
+
+    // insert data dari input pakai cin
+    int b;
+    cout << "masukan nilai: ";
+    cin >> b;
+    cout << "nilai yang anda masukan adalah: " << b << endl;
+
+    return 0;
+}
