c42669ba77296c8fb8756173e68bd17880358afe
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-06-18 14:45:28 +0700

    feat: print something

---
+++ 2-printing/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Halo sodara-sodara sekalian \n";
+    std::cout << "ini adalah baris baru.";
+    std::cout << "ini bukan baris baru.";
+    std::cout << " ini akhir" << "baru baru" << std::endl;
+
+    return 0;
+}
